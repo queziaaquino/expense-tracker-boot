66ee736dda5ff6816e5c049b8c2c481958cddc2d
queziaaquino <quezia13yasmin2014@gmail.com>
2023-05-07 14:48:44 -0300

    first commit

---
+++ src/main/java/com/example/expensetrackerreact/expensetrackerreact/ExpenseTrackerReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.expensetrackerreact.expensetrackerreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expensetrackerreact/expensetrackerreact/ExpenseTrackerReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensetrackerreact.expensetrackerreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
